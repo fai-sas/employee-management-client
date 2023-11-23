d6d92eff549a31f2a5a1cfe9e0ac18db71a310b6
fai-sas <craftedbyfaisal@gmail.com>
2023-11-23 20:51:50 +0600

    auth provider created

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import 'react-toastify/dist/ReactToastify.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import Route from './Router/Route'
+import AuthProvider from './Providers/AuthProvider'
 
 const client = new QueryClient()
 
@@ -15,9 +16,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
       <QueryClientProvider client={client}>
-        {/* <AuthProvider> */}
-        <RouterProvider router={Route}></RouterProvider>
-        {/* </AuthProvider> */}
+        <AuthProvider>
+          <RouterProvider router={Route}></RouterProvider>
+        </AuthProvider>
       </QueryClientProvider>
     </HelmetProvider>
     <ToastContainer
